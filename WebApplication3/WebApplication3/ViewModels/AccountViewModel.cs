513e9a979595941ae249276426a8e5e304ff9f0e
PieterQLovesu <pieter890@wp.pl>
2020-03-18 09:59:38 +0100

    Login Added

---
+++ WebApplication3/WebApplication3/ViewModels/AccountViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.ViewModels
+{
+    public class LoginViewModel
+    {
+       
+            [Required(ErrorMessage = "Musisz wprowadzić e-mail")]
+            [Display(Name = "Email")]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Musisz wprowadzić hasło")]
+            [DataType(DataType.Password)]
+            [Display(Name = "Hasło")]
+            public string Password { get; set; }
+
+            [Display(Name = "Zapamiętaj mnie")]
+            public bool RememberMe { get; set; }
+        
+    }
+}
